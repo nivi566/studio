6cf4ab8a8c672a4b438f4ca12d32cedabcc25b2c
Nicoll Lisseth Villamizar Muñoz <nivi566@vidalibarraquer.net>
2025-12-02 14:10:36 +0000

    puedes cambiar el logo por una caja cogida por una mano

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -13,8 +13,13 @@ export function Logo({ className }: { className?: string }) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <path d="M22 12h-6l-2 3h-4l-2-3H2" />
-            <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z" />
+            <path d="M14 9.5V5.5a1.5 1.5 0 0 0-3 0v4" />
+            <path d="M11.5 16.5a1.5 1.5 0 0 1-3 0V10" />
+            <path d="M7 16.5a1.5 1.5 0 0 1-3 0V10" />
+            <path d="M17 16.5a1.5 1.5 0 0 1-3 0V9.5" />
+            <path d="M21 14.5V10a2 2 0 0 0-2-2h-3.5a2 2 0 0 0-2 2v10.5a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V18a2 2 0 0 1 2-2h1.5a2 2 0 0 0 2-2Z" />
+            <path d="M3.5 10H8" />
+            <path d="m2 16 6-3" />
         </svg>
       <span className="text-xl font-bold tracking-tight">SwiftSend</span>
     </Link>
