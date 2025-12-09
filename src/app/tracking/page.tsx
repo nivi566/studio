ec32d61cb7eb3945d2236a556b3edbdc671b4b2f
Nicoll Lisseth Villamizar Muñoz <nivi566@vidalibarraquer.net>
2025-12-09 15:15:44 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/tracking/page.tsx
+++ src/app/tracking/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Button } from '@/components/ui/button';
