6827791d5c65b6b08d2bb6c39ff6c26161741689
Nicoll Lisseth Villamizar Muñoz <nivi566@vidalibarraquer.net>
2025-12-11 09:20:23 +0000

    tienes que cambiar el logo que esta en la parte superior e inferior. La

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("group flex items-center justify-center gap-2 text-foreground focus:outline-none focus-visible:ring-2 focus-visible:ring-ring rounded-md", className)}>
       <img 
-        src="/logo1.png"
+        src="/logo.png"
         alt="InTrack Logo"
         width={40}
         height={40}
