e1b79bf210b56faa0c774c155be1cb62f37ff339
Nicoll Lisseth Villamizar Muñoz <nivi566@vidalibarraquer.net>
2025-12-16 14:57:25 +0000

    "Vull preparar la web per pujar-la a Netlify via GitHub de manera dinàmi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,7 +8,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
